566e224b1c4b1e65758595fd6013a4ea05c81be1
Thor Lindberg <thorlindberg@icloud.com>
2022-04-09 20:10:49 +0200

    txon

--- code/txon.js
+++ code/txon.js
@@ -1,6 +1,6 @@
 const TXON = {
 
-    // return true | false == .data conforms to .init
+    // return true | false == .data validated by .init
     handshake: (json) => {
 
         const obj = JSON.parse(json)
@@ -10,6 +10,7 @@ const TXON = {
         Object.values(data).forEach(prop => {
             if (contract.hasOwnProperty(prop.type)) { // .init has type "item"
                 prop.values.forEach(val => {
+                    /*
                     console.log(
                         Object.getOwnPropertyNames(contract[prop.type]),
                         Object.getOwnPropertyNames(val)
@@ -17,6 +18,9 @@ const TXON = {
                     Object.getOwnPropertyNames(val).forEach(c => {
                         console.log(c)
                     })
+                    */
+                    console.log(Object.getOwnPropertyNames(val))
+                    console.log(Object.getOwnPropertyNames(contract[prop.type]))
                 })
             }
         })
@@ -25,18 +29,7 @@ const TXON = {
 
     },
 
-    // parses JSON then JS inits .data objects based on .init
-    initialise: (json) => {
-        const obj = JSON.parse(json)
-        // init objs as JS objets, see enumJS
-        return obj
-    },
-
     // sample TX/JSON file
     sample: '{"init":{"item":{"name":{"type":"string"},"category":{"type":"string","enum":{"largeDrink":"🍺","smallDrink":"🥤","plant":"🌱"}}}},"data":{"items":{"type":"item","values":[{"name":"DietCoke(1.5L)","category":"largeDrink"},{"name":"Fanta(0.5L)","category":"smallDrink"},{"name":"Olives","category":"plant"}]}}}',
-
-    parse: (json) => JSON.parse(json),
-
-    stringify: (obj) => JSON.stringify(obj)
 
 }
